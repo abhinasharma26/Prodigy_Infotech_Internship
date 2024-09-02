e1f2b4159e745eb309c4a39711e28be2cb443f71
Abhinash <104413177+abhinasharma26@users.noreply.github.com>
2024-09-02 22:33:43 +0530

    Delete Webscrape.py

--- Webscrape.py
+++
@@ -1,7 +0,0 @@
-import selenium
-from selenium import webdriver
-url = 'https://www.youtube.com/watch?v=YrwRd_27fhw&list=RDYrwRd_27fhw&start_radio=1'
-
-
-driver = webdriver.Chrome()
-driver.get(url)
